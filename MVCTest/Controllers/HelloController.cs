3694723596870c8c9d2c356f82ac2394eb14adf6
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-01-20 00:00:16 +0300

    Practiced rendering views

--- MVCTest/Controllers/HelloController.cs
+++ MVCTest/Controllers/HelloController.cs
@@ -6,11 +6,11 @@ public class HelloController : Controller
     // This will go to "localhost:5XXX"
     [HttpGet]
     [Route("")]
-    public string Index()
+    public ViewResult Index()
     {
-        return "Hello from the index";
+        return View();
     }
-    
+
     // Route declaration Option B
     // This will go to "localhost:5XXX/user/more"
     [HttpGet("user/more")]
@@ -18,15 +18,12 @@ public class HelloController : Controller
     {
         return "Hello user";
     }
-    
-    // Post request example
-    // This will go to "localhost:5XXX/submission"
-    [HttpPost]
-    [Route("submission")]
-    // Don't worry about what the form is doing for now. We'll get to those soon!
-    public string FormSubmission(string formInput)
+
+
+    [HttpGet("elsewhere")]
+
+    public ViewResult ElseWhere()
     {
-    	// Logic for post request here
-        return "I handled a request!";
+        return View("Index");
     }
 }
