b48a22e09dc49982163476df3525da1f8bd059e0
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-21 14:11:45 +0300

    Login_and_Reg Assignment

---
+++ Login_and_Reg/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Login_and_Reg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
