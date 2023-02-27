c6917e09d75995a120b5ed8d25734d5ceeaaa736
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-02-27 21:06:48 +0300

    Added Products and Categories

---
+++ ProductsnCategories/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProductsnCategories.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
