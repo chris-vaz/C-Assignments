a2280fb6249dd8e0f1870537bbecb016317b78a2
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-03-06 18:46:57 +0300

    Added C# and APIs

--- Csharp and APIs/APITest/Program.cs
+++ Csharp and APIs/APITest/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json")
+);
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static async Task<Pokemon> ProcessPokemonAsync(HttpClient client, string url)
+{
+    await using Stream stream =
+    await client.GetStreamAsync(url);
+    Pokemon? Result =
+        await JsonSerializer.DeserializeAsync<Pokemon>(stream);
+    return Result;
+}
+
+Pokemon OnePoke = await ProcessPokemonAsync(client, "https://pokeapi.co/api/v2/pokemon/bulbasaur");
+foreach (PokeTypes t in OnePoke.Types)
+{
+    Console.WriteLine($" - {t.Type.Name}");
+}
